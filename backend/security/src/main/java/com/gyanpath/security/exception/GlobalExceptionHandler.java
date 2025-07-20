1dd3fb5b41fe0209e9a30a2b133f892e9464a970
Tapan Kumar <90493845+tapanKr277@users.noreply.github.com>
2025-07-20 12:49:10 +0530

    Global Exception added

--- backend/security/src/main/java/com/gyanpath/security/exception/GlobalExceptionHandler.java
+++ backend/security/src/main/java/com/gyanpath/security/exception/GlobalExceptionHandler.java
@@ -21,4 +21,95 @@ public class GlobalExceptionHandler {
         customErrorMessageDto.setTimestamp(LocalDateTime.now());
         return new ResponseEntity<>(customErrorMessageDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(InvalidOtpException.class)
+    public ResponseEntity<CustomErrorMessageDto> InvalidOtpException(InvalidOtpException invalidOtpException, WebRequest webRequest){
+        CustomErrorMessageDto customErrorMessageDto = new CustomErrorMessageDto();
+        customErrorMessageDto.setError(invalidOtpException.getMessage());
+        customErrorMessageDto.setPath(webRequest.getDescription(false));
+        customErrorMessageDto.setStatus(HttpStatus.BAD_REQUEST.value());
+        customErrorMessageDto.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(customErrorMessageDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotAbleToSendEmail.class)
+    public ResponseEntity<CustomErrorMessageDto> notAbleToSendEmailException(NotAbleToSendEmail notAbleToSendEmail, WebRequest webRequest){
+        CustomErrorMessageDto customErrorMessageDto = new CustomErrorMessageDto();
+        customErrorMessageDto.setError(notAbleToSendEmail.getMessage());
+        customErrorMessageDto.setPath(webRequest.getDescription(false));
+        customErrorMessageDto.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        customErrorMessageDto.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(customErrorMessageDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(OtpNotFound.class)
+    public ResponseEntity<CustomErrorMessageDto> otpNotFoundException(OtpNotFound otpNotFound, WebRequest webRequest){
+        CustomErrorMessageDto customErrorMessageDto = new CustomErrorMessageDto();
+        customErrorMessageDto.setError(otpNotFound.getMessage());
+        customErrorMessageDto.setPath(webRequest.getDescription(false));
+        customErrorMessageDto.setStatus(HttpStatus.NOT_FOUND.value());
+        customErrorMessageDto.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(customErrorMessageDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PasswordMisMatchException.class)
+    public ResponseEntity<CustomErrorMessageDto> passwordMisMatchException(PasswordMisMatchException passwordMisMatchException, WebRequest webRequest){
+        CustomErrorMessageDto customErrorMessageDto = new CustomErrorMessageDto();
+        customErrorMessageDto.setError(passwordMisMatchException.getMessage());
+        customErrorMessageDto.setPath(webRequest.getDescription(false));
+        customErrorMessageDto.setStatus(HttpStatus.BAD_REQUEST.value());
+        customErrorMessageDto.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(customErrorMessageDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomErrorMessageDto> resourceNotFoundException(ResourceNotFoundException resourceNotFoundException, WebRequest webRequest){
+        CustomErrorMessageDto customErrorMessageDto = new CustomErrorMessageDto();
+        customErrorMessageDto.setError(resourceNotFoundException.getMessage());
+        customErrorMessageDto.setPath(webRequest.getDescription(false));
+        customErrorMessageDto.setStatus(HttpStatus.NOT_FOUND.value());
+        customErrorMessageDto.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(customErrorMessageDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(SamePasswordException.class)
+    public ResponseEntity<CustomErrorMessageDto> samePasswordException(SamePasswordException samePasswordException, WebRequest webRequest){
+        CustomErrorMessageDto customErrorMessageDto = new CustomErrorMessageDto();
+        customErrorMessageDto.setError(samePasswordException.getMessage());
+        customErrorMessageDto.setPath(webRequest.getDescription(false));
+        customErrorMessageDto.setStatus(HttpStatus.NOT_FOUND.value());
+        customErrorMessageDto.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(customErrorMessageDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<CustomErrorMessageDto> userNotFoundException(UserNotFoundException userNotFoundException, WebRequest webRequest){
+        CustomErrorMessageDto customErrorMessageDto = new CustomErrorMessageDto();
+        customErrorMessageDto.setError(userNotFoundException.getMessage());
+        customErrorMessageDto.setPath(webRequest.getDescription(false));
+        customErrorMessageDto.setStatus(HttpStatus.NOT_FOUND.value());
+        customErrorMessageDto.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(customErrorMessageDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserNotVerifiedException.class)
+    public ResponseEntity<CustomErrorMessageDto> userNotVerifiedException(UserNotVerifiedException userNotVerifiedException, WebRequest webRequest){
+        CustomErrorMessageDto customErrorMessageDto = new CustomErrorMessageDto();
+        customErrorMessageDto.setError(userNotVerifiedException.getMessage());
+        customErrorMessageDto.setPath(webRequest.getDescription(false));
+        customErrorMessageDto.setStatus(HttpStatus.BAD_REQUEST.value());
+        customErrorMessageDto.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(customErrorMessageDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(WrongPasswordException.class)
+    public ResponseEntity<CustomErrorMessageDto> wrongPasswordException(WrongPasswordException wrongPasswordException, WebRequest webRequest){
+        CustomErrorMessageDto customErrorMessageDto = new CustomErrorMessageDto();
+        customErrorMessageDto.setError(wrongPasswordException.getMessage());
+        customErrorMessageDto.setPath(webRequest.getDescription(false));
+        customErrorMessageDto.setStatus(HttpStatus.BAD_REQUEST.value());
+        customErrorMessageDto.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(customErrorMessageDto, HttpStatus.BAD_REQUEST);
+    }
+
 }
